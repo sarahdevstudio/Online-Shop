616200fc5ebd7a8968982f106c32beb21e39f95e
Sarah Hosseini <Shaghayeghhoseini57@gmail.com>
2026-09-09 16:36:15 +0330

    [Create] user.py

---
+++ model/user.py
@@ -0,0 +1,10 @@
+class User:
+
+    def __init__(self, id, username, password, name, phone, address, role):
+        self.id = id
+        self.username = username
+        self.password = password
+        self.name = name
+        self.phone = phone
+        self.address = address
+        self.role = role
