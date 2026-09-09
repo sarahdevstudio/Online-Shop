6af69d4618a60f70e9ce9af884a6919f482c1e0d
Sarah Hosseini <Shaghayeghhoseini57@gmail.com>
2026-09-09 16:39:00 +0330

    [Create] product.py

---
+++ model/product.py
@@ -0,0 +1,8 @@
+class Product:
+
+    def __init__(self, id, name, category, price, stock):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.price = price
+        self.stock = stock
